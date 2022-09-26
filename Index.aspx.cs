ec5304985579dde503aba22671c92482c7db6ab9
natferlima <ferlima.natalia@gmail.com>
2022-09-25 23:21:27 -0300

    carrega infos no dropdown de tipo e no de situação do cliente

--- Index.aspx.cs
+++ Index.aspx.cs
@@ -25,6 +25,8 @@ namespace clientsapi
         protected void Page_Load(object sender, EventArgs e)
         {
             GetClients();
+            GetClientSituations();
+            GetClientTypes();
         }
 
         private void GetClients()
@@ -71,6 +73,56 @@ namespace clientsapi
             }
             
 
+        }
+
+        private void GetClientSituations()
+        {
+            HttpResponseMessage response = httpClient.GetAsync("api/clientsituation/").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var clientSituations = response.Content.ReadAsAsync<IEnumerable<ClientSituation>>().Result;
+                var clientSituationsList = clientSituations
+                    .Select(clientSituation => new ListItem
+                    (
+                        clientSituation.Description,
+                        clientSituation.Id.ToString()
+
+                    ));
+
+                DropDownList3.DataSource = clientSituationsList;
+                DropDownList3.DataBind();
+
+                ListItem itemSelect = new ListItem(" Selecione ", "-1");
+                DropDownList3.Items.Insert(0, itemSelect);
+                DropDownList3.DataValueField = "Descrição";
+                DropDownList3.DataSource = clientSituations;
+            }
+        }
+
+        private void GetClientTypes()
+        {
+            HttpResponseMessage response = httpClient.GetAsync("api/clienttype/").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var clientTypes = response.Content.ReadAsAsync<IEnumerable<ClientType>>().Result;
+                var clientTypesList = clientTypes
+                    .Select(clientType => new ListItem
+                    (
+                        clientType.Description,
+                        clientType.Id.ToString()
+
+                    ));
+
+                DropDownList1.DataSource = clientTypesList;
+                DropDownList1.DataBind();
+
+                ListItem itemSelect = new ListItem(" Selecione ", "-1");
+                DropDownList1.Items.Insert(0, itemSelect);
+                DropDownList1.DataValueField = "Descrição";
+                DropDownList1.DataSource = clientTypes;
+            }
         }
     }
 }
